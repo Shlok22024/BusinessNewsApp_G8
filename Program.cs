336582b98a505889fdbd9cdd561270da7bb18904
Shlok Goud <shlokgoud@usf.edu>
2026-04-22 11:20:33 -0400

    .Net app created
    Pending HTML code and API integration

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ namespace BusinessNewsApp
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+            builder.Services.AddHttpClient();
 
             var app = builder.Build();
 
@@ -15,20 +16,19 @@ namespace BusinessNewsApp
             if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
             app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
             app.UseRouting();
 
             app.UseAuthorization();
 
-            app.MapStaticAssets();
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}")
-                .WithStaticAssets();
+                pattern: "{controller=Home}/{action=Index}/{id?}");
 
             app.Run();
         }
